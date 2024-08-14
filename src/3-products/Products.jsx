30d12de6871f5986a4b20748da940df4e1152757
Naimur00 <aminaim4u@gmail.com>
2024-08-15 00:19:57 +0600

    add:sample card added

--- src/3-products/Products.jsx
+++ src/3-products/Products.jsx
@@ -28,7 +28,7 @@ const Products = () => {
           </select>
         </div>
       </div>
-      <div className="grid grid-cols-4 sticky top-0">
+      <div className="grid grid-cols-4 gap-10 sticky top-0">
         {/* search, sort, filter function */}
         <div className="bg-emerald-100 rounded-xl p-4 my-10 col-span-1">
           {/* Search Input */}
@@ -164,6 +164,84 @@ const Products = () => {
               <span className="absolute bg-emerald-800 size-36 -left-2 -top-10 rounded-full group-hover:scale-100 scale-0 -z-10 group-hover:duration-700 duration-500 origin-center transform transition-all"></span>
               Filter
             </button>
+          </div>
+        </div>
+        {/* Products */}
+        <div className="col-span-3 grid grid-cols-3 gap-5 my-10">
+          <div className="max-w-[350px] space-y-4 rounded-lg bg-emerald-50 p-6 shadow-lg shadow-black/50 md:w-full h-fit">
+            <img
+              width={400}
+              height={400}
+              className="h-[275px] w-[350px] rounded-lg object-cover"
+              src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="card navigate ui"
+            />
+            <div className="grid gap-2">
+              <h1 className="text-lg font-semibold ">Product Name</h1>
+              <p className="text-sm text-gray-500 dark:text-white/60">
+                This is a brief description of the product. It highlights the
+                key features and benefits.
+              </p>
+              <div className="text-lg font-semibold">$99.99</div>
+            </div>
+            <div className="flex gap-4">
+              <button className="rounded-lg bg-slate-800 px-6 py-2 text-[12px] font-semibold text-white duration-300 hover:bg-slate-950 sm:text-sm md:text-base ">
+                Add to Cart
+              </button>
+              <button className="rounded-md border border-black/50 px-4 dark:border-white dark:hover:text-slate-800 dark:hover:bg-white  py-2  duration-300 hover:bg-gray-200">
+                View Details
+              </button>
+            </div>
+          </div>
+          <div className="max-w-[350px] space-y-4 rounded-lg bg-emerald-50 p-6 shadow-lg shadow-black/50 md:w-full h-fit">
+            <img
+              width={400}
+              height={400}
+              className="h-[275px] w-[350px] rounded-lg object-cover"
+              src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="card navigate ui"
+            />
+            <div className="grid gap-2">
+              <h1 className="text-lg font-semibold ">Product Name</h1>
+              <p className="text-sm text-gray-500 dark:text-white/60">
+                This is a brief description of the product. It highlights the
+                key features and benefits.
+              </p>
+              <div className="text-lg font-semibold">$99.99</div>
+            </div>
+            <div className="flex gap-4">
+              <button className="rounded-lg bg-slate-800 px-6 py-2 text-[12px] font-semibold text-white duration-300 hover:bg-slate-950 sm:text-sm md:text-base ">
+                Add to Cart
+              </button>
+              <button className="rounded-md border border-black/50 px-4 dark:border-white dark:hover:text-slate-800 dark:hover:bg-white  py-2  duration-300 hover:bg-gray-200">
+                View Details
+              </button>
+            </div>
+          </div>
+          <div className="max-w-[350px] space-y-4 rounded-lg bg-emerald-50 p-6 shadow-lg shadow-black/50 md:w-full h-fit">
+            <img
+              width={400}
+              height={400}
+              className="h-[275px] w-[350px] rounded-lg object-cover"
+              src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="card navigate ui"
+            />
+            <div className="grid gap-2">
+              <h1 className="text-lg font-semibold ">Product Name</h1>
+              <p className="text-sm text-gray-500 dark:text-white/60">
+                This is a brief description of the product. It highlights the
+                key features and benefits.
+              </p>
+              <div className="text-lg font-semibold">$99.99</div>
+            </div>
+            <div className="flex gap-4">
+              <button className="rounded-lg bg-slate-800 px-6 py-2 text-[12px] font-semibold text-white duration-300 hover:bg-slate-950 sm:text-sm md:text-base ">
+                Add to Cart
+              </button>
+              <button className="rounded-md border border-black px-4 dark:border-white dark:hover:text-slate-800 dark:hover:bg-white  py-2  duration-300 hover:bg-gray-200">
+                View Details
+              </button>
+            </div>
           </div>
         </div>
       </div>
