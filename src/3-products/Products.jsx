2a7347d364515899306b1c803dc64fbb043a5f6f
Naimur00 <aminaim4u@gmail.com>
2024-08-15 22:55:12 +0600

    add: tanstack query

--- src/3-products/Products.jsx
+++ src/3-products/Products.jsx
@@ -1,4 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
 const Products = () => {
+  // Getting data using TanStack queries
+  const { data: products = [], isLoading } = useQuery({
+    queryKey: ["porducts"],
+    queryFn: async () => getData(),
+  });
+
+  // getting all jobs data using axios
+  const getData = async () => {
+    const data = await axios(`${import.meta.env.VITE_API_URL}/products`);
+    return data.data;
+  };
+
   return (
     <section className="mx-7 h-screen">
       <div className="flex items-center justify-between">
@@ -168,81 +183,11 @@ const Products = () => {
         </div>
         {/* Products */}
         <div className="col-span-3 grid grid-cols-3 gap-5 my-10">
-          <div className="max-w-[350px] space-y-4 rounded-lg bg-emerald-50 p-6 shadow-lg shadow-black/50 md:w-full h-fit">
-            <img
-              width={400}
-              height={400}
-              className="h-[275px] w-[350px] rounded-lg object-cover"
-              src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt="card navigate ui"
-            />
-            <div className="grid gap-2">
-              <h1 className="text-lg font-semibold ">Product Name</h1>
-              <p className="text-sm text-gray-500 dark:text-white/60">
-                This is a brief description of the product. It highlights the
-                key features and benefits.
-              </p>
-              <div className="text-lg font-semibold">$99.99</div>
-            </div>
-            <div className="flex gap-4">
-              <button className="rounded-lg bg-slate-800 px-6 py-2 text-[12px] font-semibold text-white duration-300 hover:bg-slate-950 sm:text-sm md:text-base ">
-                Add to Cart
-              </button>
-              <button className="rounded-md border border-black/50 px-4 dark:border-white dark:hover:text-slate-800 dark:hover:bg-white  py-2  duration-300 hover:bg-gray-200">
-                View Details
-              </button>
-            </div>
-          </div>
-          <div className="max-w-[350px] space-y-4 rounded-lg bg-emerald-50 p-6 shadow-lg shadow-black/50 md:w-full h-fit">
-            <img
-              width={400}
-              height={400}
-              className="h-[275px] w-[350px] rounded-lg object-cover"
-              src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt="card navigate ui"
-            />
-            <div className="grid gap-2">
-              <h1 className="text-lg font-semibold ">Product Name</h1>
-              <p className="text-sm text-gray-500 dark:text-white/60">
-                This is a brief description of the product. It highlights the
-                key features and benefits.
-              </p>
-              <div className="text-lg font-semibold">$99.99</div>
-            </div>
-            <div className="flex gap-4">
-              <button className="rounded-lg bg-slate-800 px-6 py-2 text-[12px] font-semibold text-white duration-300 hover:bg-slate-950 sm:text-sm md:text-base ">
-                Add to Cart
-              </button>
-              <button className="rounded-md border border-black/50 px-4 dark:border-white dark:hover:text-slate-800 dark:hover:bg-white  py-2  duration-300 hover:bg-gray-200">
-                View Details
-              </button>
-            </div>
-          </div>
-          <div className="max-w-[350px] space-y-4 rounded-lg bg-emerald-50 p-6 shadow-lg shadow-black/50 md:w-full h-fit">
-            <img
-              width={400}
-              height={400}
-              className="h-[275px] w-[350px] rounded-lg object-cover"
-              src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt="card navigate ui"
-            />
-            <div className="grid gap-2">
-              <h1 className="text-lg font-semibold ">Product Name</h1>
-              <p className="text-sm text-gray-500 dark:text-white/60">
-                This is a brief description of the product. It highlights the
-                key features and benefits.
-              </p>
-              <div className="text-lg font-semibold">$99.99</div>
-            </div>
-            <div className="flex gap-4">
-              <button className="rounded-lg bg-slate-800 px-6 py-2 text-[12px] font-semibold text-white duration-300 hover:bg-slate-950 sm:text-sm md:text-base ">
-                Add to Cart
-              </button>
-              <button className="rounded-md border border-black px-4 dark:border-white dark:hover:text-slate-800 dark:hover:bg-white  py-2  duration-300 hover:bg-gray-200">
-                View Details
-              </button>
-            </div>
-          </div>
+          {
+            products.map(product => {
+              
+            })
+          }
         </div>
       </div>
     </section>
--- src/main.jsx
+++ src/main.jsx
@@ -1,53 +1,55 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import App from './layout/App.jsx'
-import './index.css'
-import One from './page/One.jsx';
-import Two from './page/Two.jsx';
-import Three from './page/Three.jsx';
-import Home from './page/Home.jsx';
-import Login from './layout/Login.jsx';
-import Register from './layout/Register.jsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./layout/App.jsx";
+import "./index.css";
+import One from "./page/One.jsx";
+import Two from "./page/Two.jsx";
+import Three from "./page/Three.jsx";
+import Home from "./page/Home.jsx";
+import Login from "./layout/Login.jsx";
+import Register from "./layout/Register.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
+    element: <App />,
     children: [
       {
         index: true,
-        element: <Home/>
+        element: <Home />,
       },
       {
-        path: '/one',
-        element: <One/>
+        path: "/one",
+        element: <One />,
       },
       {
-        path: '/two',
-        element: <Two />
+        path: "/two",
+        element: <Two />,
       },
       {
-        path: '/three',
-        element: <Three/>
+        path: "/three",
+        element: <Three />,
       },
-    ]
+    ],
   },
   {
-    path: '/login',
-    element: <Login/>
+    path: "/login",
+    element: <Login />,
   },
   {
-    path: '/register',
-    element: <Register/>
-  }
+    path: "/register",
+    element: <Register />,
+  },
 ]);
 
-createRoot(document.getElementById('root')).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </StrictMode>
+);
